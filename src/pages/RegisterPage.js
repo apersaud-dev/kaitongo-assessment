278089c227c1208e22e0a7272002e2921b7d1ed5
Andrew Persaud <andreew.j.persaud@gmail.com>
2021-11-09 08:46:30 -0500

    Create a single page to import components

---
+++ src/pages/RegisterPage.js
@@ -0,0 +1,13 @@
+import RegisterForm from "../components/form/RegisterForm";
+import Hero from "../components/hero/Hero";
+
+function RegisterPage() {
+    return (
+        <main className="main">
+            <Hero />
+            <RegisterForm />
+        </main>
+    )
+}
+
+export default RegisterPage;
